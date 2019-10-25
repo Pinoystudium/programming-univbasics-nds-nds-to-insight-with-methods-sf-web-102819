a40eab88e81b5188a16d99a198138adc2559d8c5
Michael Espina <michael_espina@yahoo.com>
2019-10-25 05:58:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,8 +29,8 @@ end
 def gross_for_director(director_data)
   total= {}
   movie_index = 0
-  while movie_index < director_data[0][:movies].length do
-    total += director_data[0][:movies][:movie_index][:worldwide_gross]
+  while movie_index < director_data[:movies].length do
+    total += director_data[:movies][:movie_index][:worldwide_gross]
     movie_index += 1
   end
   total
