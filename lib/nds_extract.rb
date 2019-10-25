ab3c7736d80fa631c2a6490fe95ef71d47190b8e
Michael Espina <michael_espina@yahoo.com>
2019-10-25 03:00:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,8 +26,9 @@ end
 # using director_data as input
 
 def gross_for_director(director_data)
+  
   data = directors_totals(director_data)
-  binding.pry
+  
   grandtotal = data.sum{ |k, v| v }   
   grand_total
 end
